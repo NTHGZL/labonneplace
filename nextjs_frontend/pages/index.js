f6204bd5161a74966fe44bfaa082f8b9b85a0911
NathanGonzalez44 <nathan_gonzalez@hotmail.fr>
2021-10-26 15:48:07 +0200

    index.js + css

--- nextjs_frontend/pages/index.js
+++ nextjs_frontend/pages/index.js
@@ -5,6 +5,28 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
     <Navbar />
+    <main>
+      <div className="search-section">
+        <div className="input-container">
+
+        <select>
+          <option defaultChecked>Catégorie</option>
+          <option>Animaux</option>
+          <option>Alimentaire</option>
+          <option>Maison</option>
+          <option>Mode</option>
+          <option>Véhicule</option>
+          <option>Multimédias</option>
+          <option>Services</option>
+        </select>
+        <input className="search-bar" />
+        <div className="price">Prix</div>
+        </div>
+        <button className="submit">Rechercher</button>
+      </div>
+    </main>
+    </>
   )
 }
