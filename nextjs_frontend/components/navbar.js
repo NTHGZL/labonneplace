9e567711e0beb740ac604d4ee8dcf3730adb68eb
NathanGonzalez44 <nathan_gonzalez@hotmail.fr>
2021-10-26 14:46:32 +0200

    add navbar component

---
+++ nextjs_frontend/components/navbar.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export default function Navbar () {
+    
+    return (
+        
+        <header>
+            <div className="logo">
+                <h1>LA BONNE PLACE</h1>
+            </div>
+            <div className="user-div">
+                <div className="avatar">
+                    NG
+                </div>
+                <div className="fullname">
+                    Nathan Gonzalez
+                </div>
+            </div>
+        </header>
+        
+    )
+}
+
+
